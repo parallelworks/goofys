ae35b700320126d344afa69170a210f35af9fe68
Louis Le <72033416+locle2302@users.noreply.github.com>
2026-08-10 10:54:27 -0500

    feat: reload profile credentials when the shared credentials file changes (#6)

--- api/common/conf_s3.go
+++ api/common/conf_s3.go
@@ -84,6 +84,8 @@ func (c *S3Config) ToAwsConfig(flags *FlagStorage) (*aws.Config, error) {
 	if c.Credentials == nil {
 		if c.AccessKey != "" {
 			c.Credentials = credentials.NewStaticCredentials(c.AccessKey, c.SecretKey, "")
+		} else if c.Profile != "" {
+			c.Credentials = newSharedFileCredentials(c.Profile)
 		}
 	}
 	if flags.Endpoint != "" {
